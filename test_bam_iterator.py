6a95a007a0bd2f3ceb3f99c2075b3b7f6d13e3ba
Ruben Vorderman <RubenVorderman@xs4all.nl>
2022-01-19 17:27:35 +0100

    Fix test script

--- test_bam_iterator.py
+++ test_bam_iterator.py
@@ -1,9 +1,10 @@
 from pybam._bamrecord import bam_iterator
-import zlib
+import gzip
+
 with open("tests/no_hdr_sq_1.bam", "rb") as test_file:
     # Try to find start position
-    test_data = zlib.decompress(test_file.read(), wbits=31)[280:]
-print(test_data)
+    test_data = gzip.decompress(test_file.read())
 
-bamrecords = bam_iterator(test_data)
+bamrecords = bam_iterator(test_data[268:])
 first = next(bamrecords)
+print(first.read_name)
