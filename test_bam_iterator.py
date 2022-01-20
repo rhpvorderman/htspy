751da49d5955adb71faee9d25888465090498aae
Ruben Vorderman <RubenVorderman@xs4all.nl>
2022-01-20 09:30:21 +0100

    Test with file from CLI

--- test_bam_iterator.py
+++ test_bam_iterator.py
@@ -1,8 +1,7 @@
 from pybam.bam import BamReader
-import gzip
-
+import sys
 if __name__ == "__main__":
-    with BamReader("tests/no_hdr_sq_1.bam") as bam_reader:
+    with BamReader(sys.argv[1]) as bam_reader:
         for record in bam_reader:
             pass
 
