992dfd30f03c225f0d975a184def33a7c00db1a4
Ruben Vorderman <rubenvorderman@xs4all.nl>
2022-01-27 16:23:23 +0100

    Rename benchmark scripts

--- pybam_read.py
+++ pybam_read.py
@@ -1,5 +1,7 @@
 from pybam.bam import BamReader
 import sys
+
+
 if __name__ == "__main__":
     with BamReader(sys.argv[1]) as bam_reader:
         for record in bam_reader:
--- test_pysam_speed.py
+++
@@ -1,7 +0,0 @@
-import pysam
-import sys
-
-if __name__ == "__main__":
-    bam = pysam.AlignmentFile(sys.argv[1])
-    for record in bam:
-        pass
