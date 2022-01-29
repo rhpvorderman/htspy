15dd163ea35a6c1c0eecde4874eb823345ce49fd
Ruben Vorderman <RubenVorderman@xs4all.nl>
2022-01-29 12:58:20 +0100

    Add profiler for writing

---
+++ profile_pybam_write.py
@@ -0,0 +1,16 @@
+from pybam.bam import BamReader, BamWriter
+import sys
+
+import cProfile
+
+
+def main():
+    with BamReader(sys.argv[1]) as bam_reader:
+        with BamWriter(sys.argv[2],
+                       bam_reader.header) as bam_writer:
+            for record in bam_reader:
+                bam_writer.write(record)
+
+
+if __name__ == "__main__":
+    cProfile.run("main()")
