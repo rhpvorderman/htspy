60e3c5d07f74701c203b6eefcd561f5ad5bae536
Ruben Vorderman <RubenVorderman@xs4all.nl>
2022-01-30 15:53:14 +0100

    Further test code

--- tests/test_pybam.py
+++ tests/test_pybam.py
@@ -3,14 +3,18 @@ import pytest
 from pybam.bam import bam_iterator
 
 
-@pytest.fixture
-def raw_bam_record() -> bytes:
+def test_bam_parsing() -> bytes:
+    reference_id = -1
+    pos = -1
+    mapq = 99
+    bin = 1001  # TODO get realistic value
+
+
     # Seq consists of 7 AA characters
     seq = b'\x11\x11\x11\x10'
     # Quality is 35 for all bases
     quals = b'#######'
     l_seq = 7
-
 
     return b""
 
