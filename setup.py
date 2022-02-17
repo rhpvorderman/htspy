2aeab0227aab1e994cf769a7ac0b9b3513ab2c63
Ruben Vorderman <RubenVorderman@xs4all.nl>
2022-02-17 07:43:21 +0100

    Add C language classifier

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: C",
         "License :: OSI Approved :: MIT License",
     ],
     python_requires=">=3.6",
