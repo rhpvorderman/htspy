c01349419d10405e601252e431980f78c1da690a
Ruben Vorderman <RubenVorderman@xs4all.nl>
2022-02-21 07:25:00 +0100

    Add extra benchmarks for pysam

---
+++ benchmarks/bam_read_and_write_uncompressed.py
@@ -0,0 +1,10 @@
+from htspy.bam import BamReader, BamWriter
+import sys
+
+
+if __name__ == "__main__":
+    with BamReader(sys.argv[1]) as bam_reader:
+        with BamWriter(sys.argv[2],
+                       bam_reader.header, compresslevel=0) as bam_writer:
+            for record in bam_reader:
+                bam_writer.write(record)
